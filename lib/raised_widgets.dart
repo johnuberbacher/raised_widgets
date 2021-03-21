a9b1edf1a4fd586407e0fd3d870b57c3a103cbd2
John Uberbacher <johnuberbacher@gmail.com>
2021-03-21 11:33:46 -0600

    First package commit

--- lib/raised_widgets.dart
+++ lib/raised_widgets.dart
@@ -1,7 +1,56 @@
 library raised_widgets;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class RaisedWidgetStyle {
+  final Color topColor;
+  final Color backColor;
+  final BorderRadius borderRadius;
+  final double distance;
+  const RaisedWidgetStyle({
+    this.topColor = const Color(0xFF45484c),
+    this.backColor = const Color(0xFF191a1c),
+    this.borderRadius = const BorderRadius.all(Radius.circular(7.0)),
+    this.distance = 15.0,
+  });
+}
+
+class RaisedWidget extends StatelessWidget {
+  final Widget child;
+  final RaisedWidgetStyle style;
+
+  RaisedWidget({@required this.child, this.style});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: AlignmentDirectional.topCenter,
+      children: <Widget>[
+        DecoratedBox(
+          position: DecorationPosition.background,
+          decoration: BoxDecoration(
+              borderRadius: style.borderRadius,
+              boxShadow: [BoxShadow(color: style.backColor)]),
+          child: Container(),
+        ),
+        Padding(
+          padding: EdgeInsets.only(
+            bottom: style.distance,
+          ),
+          child: DecoratedBox(
+            position: DecorationPosition.background,
+            decoration: BoxDecoration(
+                borderRadius: style.borderRadius,
+                boxShadow: [BoxShadow(color: style.topColor)]),
+            child: Container(
+              width: double.infinity,
+              padding: EdgeInsets.zero,
+              alignment: Alignment.center,
+              child: child,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
 }
